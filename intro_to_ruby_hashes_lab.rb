1dc7da30840b1f71994f675ef61cf65a715befed
Olga Rosas <olga.rosas@gmail.com>
2019-06-05 16:26:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,8 +28,8 @@ def monopoly_with_third_tier
   monopoly[:railroads][:pieces] = 4
 	monopoly[:railroads][:names] = {}
 	monopoly[:railroads][:rent_in_dollars] = {}
-	#monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
-
+	monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end

bc69f0959d559682a1ed2582cf2ef70104b26f0f
Olga Rosas <olga.rosas@gmail.com>
2019-06-05 16:35:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -57,5 +57,6 @@ def monopoly_with_fourth_tier
 	#copy and paste the monopoly hash you build in the previous method. We're going to add stuff to it here!
 	monopoly[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
 	monopoly[:railroads][:names][:pennsylvania_railroad]["mortgage_value"] = "$200"
+	monopoly[:railroads][:names][:b_and_o_railroad]["mortgage_value"] = "$400"
 monopoly
 end

7af474f0bd31bb979142e8fc1895137e17d9854d
github username <github email address>
2019-06-05 20:19:51 -0400

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,13 +42,14 @@ def monopoly_with_third_tier
 				three_pieces_owned: 100,
 				four_pieces_owned: 200
 			},
-		},
-		names: {
-			reading_railroad: {},
-			pennsylvania_railroad: {},
-			b_and_o_railroad: {},
-			shortline: {}
-		}
+
+			names: {
+					reading_railroad: {},
+					pennsylvania_railroad: {},
+					b_and_o_railroad: {},
+					shortline: {}
+			}
+		}	
 
 	}
 
@@ -65,21 +66,16 @@ def monopoly_with_fourth_tier
 				three_pieces_owned: 100,
 				four_pieces_owned: 200
 			},
-		},
-		names: {
-			reading_railroad: {
-				'mortgage_value' => '$100'
-			},
-			pennsylvania_railroad: {
-				'mortgage_value' => '$200'
-			},
-			b_and_o_railroad: {
-				"mortgage_value" => '$400'
-			},
-			shortline: {
-				"mortgage_value" => '$800'
+
+			names: {
+				reading_railroad: {'mortgage_value' => '$100'},
+				pennsylvania_railroad: {'mortgage_value' => '$200'},
+				b_and_o_railroad: {'mortgage_value' => '$400'},
+				shortline: {'mortgage_value' => '$800'}
 			}
-		}
+		}	
+
+	
 
 	}
 end

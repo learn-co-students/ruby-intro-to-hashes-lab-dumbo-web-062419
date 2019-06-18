49c9e1dc189047568124e63b5fbf3881a63978d3
Aliciea D Suechin <adr330@nyu.edu>
2019-06-18 14:24:43 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,15 @@
-def new_hash
-
+def new_hash 
+  new_hash = {}
 end
 
-def actor
 
-end
+def actor 
+  actor = { name: 'Dwayne The Rock Johnson'
+}
+end 
 
 def monopoly
-	monopoly = {}
+	monopoly[:railroads] = {}
 
 end
 
